192c1f2b77a81e44ee0829984bf0b8faded4cf5e
unknown <soumya.ray2472@gmail.com>
2024-03-24 22:16:59 +0530

    Made the frontend basic

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="bg-green-300 text-white  h-dvh">
+        Hello World
+      {/* <LeftSide></LeftSide>
+      <RightSide></RightSide> */}
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/LeftSide.jsx
@@ -0,0 +1,13 @@
+export const LeftSide = () => {
+    return (
+        <>
+            <div>Welcome to</div>
+            <div>Soumya's</div> 
+            <div>Newsletter</div>
+            <div>
+                <button>Login</button>
+                <button>Signup</button>
+            </div>
+        </>
+    )
+}
